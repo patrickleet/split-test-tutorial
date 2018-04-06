627251594b74c4a95e14d3b9e66e32cb97384ee9
Patrick Scott <pat@patscott.io>
2018-04-06 16:17:53 -0400

    connect redux

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,9 @@
-export default () => <div>Welcome to next.js!</div>
+import initStore from '../lib/initRedux'
+import withRedux from 'next-redux-wrapper'
+
+const Index = () => <div>Welcome to next.js!</div>
+
+const mapStateToProps = () => ({})
+const mapDispatchToProps = () => ({})
+
+export default withRedux(initStore, mapStateToProps, mapDispatchToProps)(Index)
