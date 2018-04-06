896d0c0a5d4a841a0c04e69c31433ce4dbe568cf
Patrick Scott <pat@patscott.io>
2018-04-06 16:47:21 -0400

    access dispatch from getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,16 @@ import withRedux from 'next-redux-wrapper'
 
 const Index = () => <div>Welcome to next.js!</div>
 
+/* context: {req, res, query, isServer, store} */
+Index.getInitialProps = ({store}) => {
+  const dispatchStartExperiment = ({name, variant}) => {
+    store.dispatch(startExperiment({
+      name,
+      variant
+    }))
+  }
+}
+
 const mapStateToProps = () => ({})
 const mapDispatchToProps = () => ({})
 
